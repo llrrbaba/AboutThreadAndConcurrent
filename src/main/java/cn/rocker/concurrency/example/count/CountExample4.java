25e2abfbd03d5ad767a820d76a37cfb352033c63
llrrbaba <rocker_pg@163.com>
2019-01-08 17:11:13 +0800

    volatile可以保证可见性和禁止指令重排序，但是不能保证变量的线程安全

---
+++ src/main/java/cn/rocker/concurrency/example/count/CountExample4.java
@@ -0,0 +1,64 @@
+package cn.rocker.concurrency.example.count;
+
+
+import cn.rocker.concurrency.annotations.NotThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author rocker
+ * @date 2019/01/08 14:33
+ * @since V1.0
+ */
+@NotThreadSafe
+public class CountExample4 {
+
+    private static final Logger logger = LoggerFactory.getLogger(CountExample4.class);
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //并发的线程数
+    public static int threadTotal = 200;
+
+    public static volatile int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        //创建线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //定义countDownLatch
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i=0;i<clientTotal;i++){
+            executorService.execute(new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        add();
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        logger.error("exception:{}", e);
+                    }
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        //等待所有请求执行完毕，输出count值
+        countDownLatch.await();
+        executorService.shutdown();
+        logger.info("count:{}", count);
+    }
+
+    private static void add(){
+        count++;
+    }
+
+}
