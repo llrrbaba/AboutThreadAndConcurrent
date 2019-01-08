0315ccbaee217d1440ea3f039fe18dee084dab4e
llrrbaba <rocker_pg@163.com>
2019-01-08 15:25:03 +0800

    原子类

---
+++ src/main/java/cn/rocker/concurrency/example/atomic/AtomicExample2.java
@@ -0,0 +1,66 @@
+package cn.rocker.concurrency.example.atomic;
+
+
+import cn.rocker.concurrency.annotations.ThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * AtomicLong
+ * @author rocker
+ * @date 2019/01/08 14:33
+ * @since V1.0
+ */
+@ThreadSafe
+public class AtomicExample2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(AtomicExample2.class);
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //并发的线程数
+    public static int threadTotal = 200;
+
+    public static AtomicLong count = new AtomicLong(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        //创建线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //定义countDownLatch
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i=0;i<clientTotal;i++){
+            executorService.execute(new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        add();
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        logger.error("exception:{}", e);
+                    }
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        //等待所有请求执行完毕，输出count值
+        countDownLatch.await();
+        executorService.shutdown();
+        logger.info("count:{}", count.get());
+    }
+
+    private static void add(){
+        count.incrementAndGet();
+    }
+
+}
