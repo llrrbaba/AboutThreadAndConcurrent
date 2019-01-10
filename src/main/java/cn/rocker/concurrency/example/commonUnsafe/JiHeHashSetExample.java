e09628423623c1a28001e18a375542c9bbde25bb
llrrbaba <rocker_pg@163.com>
2019-01-10 17:46:56 +0800

    线程不安全的集合类演示

---
+++ src/main/java/cn/rocker/concurrency/example/commonUnsafe/JiHeHashSetExample.java
@@ -0,0 +1,70 @@
+package cn.rocker.concurrency.example.commonUnsafe;
+
+import cn.rocker.concurrency.annotations.NotThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * TODO 研究下 HashSet在add时线程不安全的底层原理
+ * @author rocker
+ * @date 2019/01/10 16:38
+ * @since V1.0
+ */
+@NotThreadSafe
+public class JiHeHashSetExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(JiHeHashSetExample.class);
+
+    private static Set<Integer> set = new HashSet<>();
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //并发的线程数
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        //创建线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //定义countDownLatch
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i=0;i<clientTotal;i++){
+            final int count = i;
+            executorService.execute(new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        update(count);
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        logger.error("exception:{}", e);
+                    }
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        //等待所有请求执行完毕，输出count值
+        countDownLatch.await();
+        executorService.shutdown();
+
+        logger.info("size:{}", set.size());
+    }
+
+    private static void update(int count){
+        set.add(count);
+    }
+
+}
