340cbf94fe65f4063debf6ee015403b2690823a2
apple <rocker_pg@163.com>
2019-01-12 10:49:00 +0800

    stampedlock

--- src/main/java/cn/rocker/concurrency/example/lock/LockExample4.java
+++ src/main/java/cn/rocker/concurrency/example/lock/LockExample4.java
@@ -1,32 +1,36 @@
-package cn.rocker.concurrency.example.syncContainer;
+package cn.rocker.concurrency.example.lock;
 
-import cn.rocker.concurrency.annotations.NotThreadSafe;
+
+import cn.rocker.concurrency.annotations.ThreadSafe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Vector;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.StampedLock;
 
 /**
  * @author rocker
- * @date 2019/01/10 16:38
+ * @date 2019/01/08 14:33
  * @since V1.0
  */
-@NotThreadSafe
-public class JiHeVectorExample {
+@ThreadSafe
+public class LockExample4 {
 
-    private static final Logger logger = LoggerFactory.getLogger(JiHeVectorExample.class);
+    private static final Logger logger = LoggerFactory.getLogger(LockExample4.class);
 
-    private static List<Integer> list = new Vector<>();
+    private static final StampedLock lock = new StampedLock();
 
     //请求总数
     public static int clientTotal = 5000;
     //并发的线程数
     public static int threadTotal = 200;
+
+    public static int count = 0;
 
     public static void main(String[] args) throws InterruptedException {
         //创建线程池
@@ -37,13 +41,12 @@ public class JiHeVectorExample {
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
 
         for(int i=0;i<clientTotal;i++){
-            final int count = i;
             executorService.execute(new Thread(){
                 @Override
                 public void run() {
                     try {
                         semaphore.acquire();
-                        update(count);
+                        add();
                         semaphore.release();
                     } catch (InterruptedException e) {
                         logger.error("exception:{}", e);
@@ -56,12 +59,16 @@ public class JiHeVectorExample {
         //等待所有请求执行完毕，输出count值
         countDownLatch.await();
         executorService.shutdown();
-
-        logger.info("size:{}", list.size());
+        logger.info("count:{}", count);
     }
 
-    private static void update(int count){
-        list.add(count);
+    private static void add(){
+        long stamp = lock.writeLock();
+        try {
+            count++;
+        } finally {
+            lock.unlock(stamp);
+        }
     }
 
 }
